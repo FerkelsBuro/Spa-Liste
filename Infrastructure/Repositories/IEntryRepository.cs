9dfaf62e4053f7858a887835dfa1f640aad1eb4e
ll9 <llwierer@web.de>
2020-03-31 13:54:53 +0200

    ientry repostiry

---
+++ Infrastructure/Repositories/IEntryRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models;
+using System.Collections.Generic;
+
+namespace Infrastructure.Repositories
+{
+    public interface IEntryRepository
+    {
+        IEnumerable<Entry> List();
+
+        void Update(Entry entry);
+
+        void Add(Entry entry);
+
+        void Delete(Entry entry);
+
+        void SaveChanges();
+    }
+}
