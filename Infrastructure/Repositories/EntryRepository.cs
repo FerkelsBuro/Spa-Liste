6f7cc2d24878bd6e3ba2812caf5c74cd7aba2162
ll9 <llwierer@web.de>
2020-03-31 14:00:18 +0200

    declared entry repo

---
+++ Infrastructure/Repositories/EntryRepository.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Models;
+using System.Collections.Generic;
+
+namespace Infrastructure.Repositories
+{
+    public class EntryRepository : IEntryRepository
+    {
+        public void Add(Entry entry)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(Entry entry)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Entry> List()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Entry entry)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
